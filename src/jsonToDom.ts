4e37b29e3763aceba39feb8efc89ce8a2e9912c8
robin jacobsson <robin.jacobsson@kebne.se>
2024-01-27 18:20:42 +0100

    Change order of wrap, add pre last

--- src/jsonToDom.ts
+++ src/jsonToDom.ts
@@ -12,54 +12,52 @@ export type JSONValue =
   | JSONArray;
 
 export type Styling = {
-  field?: string;
-  number?: string;
-  string?: string;
-  null?: string;
-  boolean?: string;
-  braces?: string;
-  brackets?: string;
+  Field?: string;
+  Number?: string;
+  String?: string;
+  Null?: string;
+  Boolean?: string;
+  Braces?: string;
+  Brackets?: string;
 };
 
 const styling = {
-  field: "gray",
-  number: "blue",
-  string: "green",
-  // null: "teal",
-  boolean: "purple",
-  braces: "orange",
-  brackets: "red",
+  Field: "gray",
+  Number: "lightBlue",
+  String: "green",
+  Null: "teal",
+  Boolean: "purple",
+  Braces: "orange",
+  Brackets: "red",
 };
 
 const pre = (styling: Styling, text: string) => {
-  const entries = Object.entries(styling);
-
-
-
-  const css = entries
+  const css = Object.entries(styling)
     .map(([field, color]) => `--json-to-dom-${field}:${color};`)
     .join("");
 
-  console.log(css);
-
-  return `<pre style="${css}">${text}</pre>`;
+  return `<pre style="position: relative; margin:0; ${css}">${text}</pre>`;
 };
 
 const span = (variable: string) => (value: string) =>
-  `<span style="color: var(${variable})">${value}</span>`;
+  `<span style="positon: relative; color: var(${variable})">${value}</span>`;
 
 const toString = (json: JSONValue, space?: number) =>
   JSON.stringify(json, undefined, space ?? 2);
 
 export const toJsonHtmlString = (json: JSONValue, space?: number) => {
-  return pre(styling, toString(json, space))
-    .replace(/\{/g, span("--json-to-dom-braces"))
-    .replace(/\}/g, span("--json-to-dom-braces"))
-    .replace(/\[/g, span("--json-to-dom-brackets"))
-    .replace(/\]/g, span("--json-to-dom-brackets"))
-    .replace(/\b\d+(\.\d+)?\b(?![^-]*-)/g, span("--json-to-dom-number"))
-    .replace(/\b(?:true|false)\b/g, span("--json-to-dom-boolean"))
-    // .replace(/'[^']*'|"[^"]*"/g, span("--json-to-dom-string"))
-    .replace(/\bnull\b/g, span("--json-to-dom-null"))
-    .replace(/"([^"]*)"\s*:/g, span("--json-to-dom-field"));
+  const withSpans = toString(json, space)
+    .replace(/\{/g, span("--json-to-dom-Braces"))
+    .replace(/\}/g, span("--json-to-dom-Braces"))
+    .replace(/\[/g, span("--json-to-dom-Brackets"))
+    .replace(/\]/g, span("--json-to-dom-Brackets"))
+    .replace(
+      /(?<![\w"])(-?\b0*[1-9]\d*(\.\d+)?\b)(?![\w"])/g,
+      span("--json-to-dom-Number")
+    )
+    .replace(/\b(?:true|false)\b/g, span("--json-to-dom-Boolean"))
+    // .replace(/'[^']*'|"[^"]*"/g, span("--json-to-dom-String"))
+    .replace(/\bnull\b/g, span("--json-to-dom-Null"))
+    .replace(/"([^"]*)"\s*:/g, span("--json-to-dom-Field"));
+  return pre(styling, withSpans);
 };
