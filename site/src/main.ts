decef9731d7bcb3ba86f4c46c1f8c37b3f1a9594
robin jacobsson <robin.jacobsson@kebne.se>
2024-02-19 12:37:28 +0100

    Remove files not needed

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,8 +1,5 @@
-import './app.css'
-import App from './App.svelte'
+import App from "./App.svelte";
 
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
+new App({
+  target: document.getElementById("app") as HTMLElement,
+});
