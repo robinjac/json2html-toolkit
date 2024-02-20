247562ce1a956df4b877e69e18a590614e379a8e
robin jacobsson <robin.jacobsson@kebne.se>
2024-02-20 16:39:58 +0100

    Site: Animate colors

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { toHtmlString } from "json2html-toolkit";
-  import Typewriter from "svelte-typewriter";
+  import { tweened } from "svelte/motion";
+  import { onMount } from "svelte";
+  import { cubicInOut } from "svelte/easing";
   import { randomHue, complementaryHue, averageHue } from "./utils";
 
   const json = {
@@ -14,34 +16,44 @@
     },
   };
 
+  const sections = 3;
+
   // Generate a random base hue
   let baseHue = randomHue();
 
-  const sections = 3;
+  // Generate a random base hue
+  let hues = tweened(
+    [0, 1, 2].map(() => Math.round(baseHue + (80 - Math.random() * 80))),
+    {
+      duration: 1000,
+      easing: cubicInOut,
+    }
+  );
 
-  // Calculate hues for the palette
-  let hues: number[] = []; // Include the complementary hue
-  for (let i = 0; i < sections; i++) {
-    hues.push(baseHue + (80 - Math.random() * 80));
+  let colors = ["", "", "", "", ""];
+
+  $: {
+    const avgHue = averageHue($hues);
+    const hue_ = Math.round(complementaryHue(avgHue));
+
+    colors = [...$hues, hue_, hue_ + 30, complementaryHue(hue_ + 30)].map(
+      (hue, i) => `hsl(${hue}, ${i > sections - 1 ? 50 : 100}%, 50%)`
+    );
   }
 
-  const avgHue = averageHue(hues);
-  const hue_ = complementaryHue(avgHue);
-
-  // Convert hues to RGB
-  let colors = [...hues, hue_, hue_ + 30, complementaryHue(hue_ + 30)].map(
-    (hue, i) => `hsl(${hue}, ${i > sections - 1 ? 50 : 100}%, 50%)`
-  );
+  onMount(() => {
+    setInterval(() => {
+      $hues = $hues.map(hue =>hue + 10)
+    }, 1000);
+  });
 </script>
 
-<Typewriter mode="cascade" interval={60}>
-  <div
-    class="json-box"
-    style={`--color1: ${colors[0]}; --color2: ${colors[1]}; --color3: ${colors[2]};  --gradient-from: ${colors[3]}; --gradient-to: ${colors[4]}; --border-color: ${colors[5]}`}
-  >
-    {@html toHtmlString(json, { space: 4 })}
-  </div>
-</Typewriter>
+<div
+  class="json-box"
+  style={`--color1: ${colors[0]}; --color2: ${colors[1]}; --color3: ${colors[2]};  --gradient-from: ${colors[3]}; --gradient-to: ${colors[4]}; --border-color: ${colors[5]}`}
+>
+  {@html toHtmlString(json, { space: 4 })}
+</div>
 
 <code class="copy-box">npm install json2html-toolkit</code>
 
@@ -83,4 +95,16 @@
     --json2html-comma: var(--color3);
     --json2html-semi: var(--color3);
   }
+
+  /* 
+  Nice colors
+  style attribute {
+    --color1: hsl(33.93151372274401, 100%, 50%);
+    --color2: hsl(27.104123282106173, 100%, 50%);
+    --color3: hsl(72.88356962304904, 100%, 50%);
+    --gradient-from: hsl(224.35740833829504, 50%, 50%);
+    --gradient-to: hsl(254.35740833829504, 50%, 50%);
+    --border-color: hsl(74.35740833829504, 50%, 50%);
+  
+  */
 </style>
